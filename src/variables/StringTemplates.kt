2b16912d8461b83a5b09afb45cf7b8ead8b589b2
devingk <devcharmingk@naver.com>
2024-06-26 20:52:12 +0900

    String templates

---
+++ src/variables/StringTemplates.kt
@@ -0,0 +1,9 @@
+package variables
+
+fun main() {
+    val customers = 20
+    println("There are $customers customers")
+
+    println("There are $customers + 10 customers")
+    println("There are ${customers + 10} customers")
+}
