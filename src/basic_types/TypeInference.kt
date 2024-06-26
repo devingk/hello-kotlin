cf083663c2fef517a2b37f1141a2f7a468e2f879
devingk <devcharmingk@naver.com>
2024-06-26 20:57:54 +0900

    Basic types

---
+++ src/basic_types/TypeInference.kt
@@ -0,0 +1,20 @@
+package basic_types
+
+fun main() {
+    var customers = 10
+
+    customers = 8
+    println(customers)
+
+    customers += 10
+    println(customers)
+
+    customers -= 10
+    println(customers)
+
+    customers *= 10
+    println(customers)
+
+    customers /= 10
+    println(customers)
+}
---
+++ src/basic_types/TypeSpecification.kt
@@ -0,0 +1,12 @@
+package basic_types
+
+fun main() {
+
+    val d: Int
+    d = 10;
+
+    val e: String = "hello"
+
+    println(d)
+    println(e)
+}
