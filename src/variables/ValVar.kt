3c33f00184f23d80391e72af248ae3fde81726fc
devingk <devcharmingk@naver.com>
2024-06-26 20:47:32 +0900

    val, var keyword

---
+++ src/variables/ValVar.kt
@@ -0,0 +1,16 @@
+package variables
+
+fun main() {
+
+    //val: read-only variable keyword
+    val a = 5;
+
+    //var: mutable variable keyword
+    var b = 10;
+
+//    a = 22;
+    b = 22;
+
+    println("a = $a")
+    println("b = $b")
+}
