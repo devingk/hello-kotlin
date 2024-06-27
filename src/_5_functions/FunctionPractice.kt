994aa01211b56865c0782b5abfcbe28ee828aecf
devingk <devcharmingk@naver.com>
2024-06-27 20:30:22 +0900

    Functions

---
+++ src/_5_functions/FunctionPractice.kt
@@ -0,0 +1,15 @@
+package _5_functions
+
+fun main() {
+//    hello()
+    println(sum(1, 2))
+}
+
+fun hello() {
+    return println("Hello, world!")
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
